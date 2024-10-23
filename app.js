c0bc33edbd450351443eda6c2013319ee096249f
MCristina0 <juagadorade2018@gmail.com>
2024-10-23 09:00:29 -0500

    decorator

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
-import { playList } from "playList.js";
+import { playList } from 'playList.js'; // Corrige la ruta de importación
 
-const audio =document.getElementById('audio') 
-const play =document.getElementById('play')
-const pause= document.getElementById('pause')
-const forward = document.getElementById('forward')
-const rewind = document.getElementById('rewind')
-const stop = document.getElementById('stop')
+const audio = document.getElementById('audio');
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const forward = document.getElementById('forward');
+const rewind = document.getElementById('rewind');
+const stop = document.getElementById('stop');
 
-play.addEventListener('click', () => audio.play())
 
-pause.addEventListener('click', () => audio.pause())
 
-rewind.addEventListener('click', () => audio.currentTime -=10)
+// Eventos de control
+play.addEventListener('click', () => audio.play());
 
-forward.addEventListener('click', () => audio.currentTime += 10)
+pause.addEventListener('click', () => audio.pause());
+
+rewind.addEventListener('click', () => {
+  audio.currentTime -= 10;
+});
+
+forward.addEventListener('click', () => {
+  audio.currentTime += 10;
+});
 
 stop.addEventListener('click', () => {
+  audio.pause();
+  audio.currentTime = 0; ``
+});
 
-audio.pause()
+let currentSongIndex = 0;
 
-audio.currentTime = 0
+const img = document.querySelector('.player__img'); 
+const title = document.querySelector('.player__song');
+const artist = document.querySelector('.player__artist');
 
-19 })
+function loadSong(song) {
+  title.textContent = song.title;
+  artist.textContent = song.artist;
+  img.src = song.img;
+  audio.src = song.song;
+}
+
+
+loadSong(playList[currentSongIndex]);
--- playlist.js
+++ playlist.js
@@ -1,38 +1,38 @@
 
- export const playList = [
+ export  const playList = [
     {
       title: 'sofia ',
       artist: 'alvaro soler',
-      img: 'ruta de la imagen de la canción',
+      img: 'img/img.jpeg',
       song: 'musica/Alvaro Soler - Sofia(MP3_128K)_1.mp3'
     },
     {
       title: 'debo saber ',
       artist: 'barbie',
-      img: 'ruta de la imagen de la canción',
+      img: 'img/img.jpeg',
       song: 'musica/Debo Saber _ Letra _ Barbie™ como _La Princesa de la Isla_(M4A_128K).m4a'
     },
     {
       title: 'ojos asi  ',
       artist: 'shakira',
-      img: 'ruta de la imagen de la canción',
+      img: 'img/img.jpeg',
       song: 'musica/Shakira - Ojos Así (Official HD Video)(MP3_160K).mp3'
     },
     {
       title: 'chk chk ',
       artist: ' stray kids',
-      img: 'ruta de la imagen de la canción',
+      img: 'img/img.jpeg',
       song: 'musica/Stray Kids _Chk Chk Boom_ Performance Video(MP3_160K).mp3'
     },
     {
       title: 'Dace ',
       artist: ' lyrics',
-      img: 'ruta de la imagen de la canción',
+      img: 'img/img.jpeg',
       song: 'musica/Tones and I - Dance Monkey (Lyrics)(MP3_128K)_1.mp3'
     },  {
         title: 'virtual diva ',
         artist: ' don omar',
-        img: 'ruta de la imagen de la canción',
+        img: 'img/img.jpeg',
         song: 'musica/virtual diva - don omar _ letra(MP3_128K).mp3'
-      },
+      }
   ]
